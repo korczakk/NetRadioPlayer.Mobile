bb24969e6df5d0d8c8a7e9225009209aa7112c35
kamil.korczak <kamil.korczak@gmail.com>
2020-01-22 22:24:02 +0100

    Check if internet connection is present

--- NetRadioPlayer.Mobile/NetRadioPlayer.Mobile/App.xaml.cs
+++ NetRadioPlayer.Mobile/NetRadioPlayer.Mobile/App.xaml.cs
@@ -1,45 +1,47 @@
-﻿using Microsoft.Azure.EventHubs;
-using Microsoft.Azure.EventHubs.Processor;
-using NetRadioPlayer.Mobile.Configuration;
-using NetRadioPlayer.Mobile.Persistance;
-using NetRadioPlayer.Mobile.Services;
-using System;
+﻿using NetRadioPlayer.Mobile.Persistance;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
+using NetRadioPlayer.Mobile.Helpers;
 
 namespace NetRadioPlayer.Mobile
 {
   public partial class App : Application
   {
-    private EventProcessorHost eventProcessorHost;
-
     public App()
     {
       InitializeComponent();
       SqliteDatabaseHandler.LoadSqliteDatabase(DependencyService.Get<IDbPath>());
-      MainPage = new MainPage();
+
+      Connectivity.ConnectivityChanged += ConnectivityChanged;
+
+      if (Connectivity.NetworkAccess == NetworkAccess.Internet)
+        MainPage = new MainPage();
+      else
+        MainPage = new NoConnectionPage();      
+    }
+
+    private async void ConnectivityChanged(object sender, ConnectivityChangedEventArgs e)
+    {
+      if (Connectivity.NetworkAccess == NetworkAccess.Internet)
+      {
+        MainPage = new MainPage();
+        await EventHubRegistrator.Register();
+      }
+      else
+      {
+        MainPage = new NoConnectionPage();
+      }
     }
 
     protected async override void OnStart()
     {
-      string consumerGroupName = DependencyService.Resolve<IConsumerGroupName>().CreateConsumerGroupName();
-
-      var eventHubName = Appsettings.Settings["EventHubName"];
-      var eventHubCN = Appsettings.Settings["EventHubConnectionString"];
-      var eventStorageCN = Appsettings.Settings["EventStorageConnectionString"];
-      var eventContainerName = Appsettings.Settings["EventStorageContainerName"];
-      eventProcessorHost = new EventProcessorHost(
-        eventHubName,
-        consumerGroupName,
-        eventHubCN,
-        eventStorageCN,
-        eventContainerName);
-      await eventProcessorHost.RegisterEventProcessorAsync<DeviceEventProcessor>();
+      if (Connectivity.NetworkAccess == NetworkAccess.Internet)
+        await EventHubRegistrator.Register();
     }
 
     protected async override void OnSleep()
     {
-      await eventProcessorHost.UnregisterEventProcessorAsync();
+      await EventHubRegistrator.Unregister();
     }
 
     protected override void OnResume()
---
+++ NetRadioPlayer.Mobile/NetRadioPlayer.Mobile/Helpers/EventHubRegistrator.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Azure.EventHubs.Processor;
+using NetRadioPlayer.Mobile.Configuration;
+using NetRadioPlayer.Mobile.Services;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace NetRadioPlayer.Mobile.Helpers
+{
+  public class EventHubRegistrator
+  {
+    private static EventProcessorHost eventProcessorHost;
+
+    public static async Task Register()
+    {
+      string consumerGroupName = DependencyService.Resolve<IConsumerGroupName>().CreateConsumerGroupName();
+
+      var eventHubName = Appsettings.Settings["EventHubName"];
+      var eventHubCN = Appsettings.Settings["EventHubConnectionString"];
+      var eventStorageCN = Appsettings.Settings["EventStorageConnectionString"];
+      var eventContainerName = Appsettings.Settings["EventStorageContainerName"];
+      eventProcessorHost = new EventProcessorHost(
+        eventHubName,
+        consumerGroupName,
+        eventHubCN,
+        eventStorageCN,
+        eventContainerName);
+      await eventProcessorHost.RegisterEventProcessorAsync<DeviceEventProcessor>();
+
+    }
+
+    public static async Task Unregister()
+    {
+      await eventProcessorHost.UnregisterEventProcessorAsync();
+    }
+  }
+}
